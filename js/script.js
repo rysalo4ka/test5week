b4c3edc17732fe5821d2b53ea1e7ed0887785c3b
rysalo4ka <97964363+rysalo4ka@users.noreply.github.com>
2022-02-17 12:28:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,6 +43,20 @@ var insertProperty = function (string, propName, propValue) {
       .replace(new RegExp(propToReplace, "g"), propValue);
     return string;
   }
+// Remove the class 'active' from home and switch to Menu button
+var switchMenuToActive = function () {
+  // Remove 'active' from home button
+  var classes = document.querySelector("#navHomeButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navHomeButton").className = classes;
+
+  // Add 'active' to menu button if not already there
+  classes = document.querySelector("#navMenuButton").className;
+  if (classes.indexOf("active") == -1) {
+    classes += " active";
+    document.querySelector("#navMenuButton").className = classes;
+  }
+};
 
   
     // On page load (before images or CSS)
